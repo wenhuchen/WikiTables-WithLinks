958918b3ef775a07c943ea85e544254eaba22f42
Wenhu Chen <hustchenwenhu@ucsb.edu>
2020-03-28 14:39:42 -0700

    update utils

--- utils.py
+++ utils.py
@@ -1,20 +1,14 @@
 from nltk.tokenize import word_tokenize, sent_tokenize
 import urllib.parse
 import sys
+import json
+import gzip
 
 def tokenize(string, remmove_dot=False):
     def func(string):
         return " ".join(word_tokenize(string))
     
     string = string.rstrip('.')
-    """
-    if maintain_dot and string.endswith('.'):
-        tmp = string.split(' ')
-        if len(tmp) <= 5 and tmp[-1][0].isupper() and string.find('.') != len(string) - 1:
-            # If the phrase is short enough
-            string = string.rstrip('.')
-            return func(string) + '.'
-    """
     return func(string)
 
 def url2dockey(string):
@@ -25,3 +19,21 @@ def filter_firstKsents(string, k):
     string = sent_tokenize(string)
     string = string[:k]
     return " ".join(string)
+
+def compressGZip(file_name):
+    with open(file_name, 'r') as f:
+        data = json.load(f)
+
+    json_str = json.dumps(data) + "\n"               # 2. string (i.e. JSON)
+    json_bytes = json_str.encode('utf-8')            # 3. bytes (i.e. UTF-8)
+
+    with gzip.GzipFile(file_name + '.gz', 'w') as fout:   # 4. gzip
+        fout.write(json_bytes)
+
+def readGZip(file_name):
+    with gzip.GzipFile(file_name, 'r') as fin:    # 4. gzip
+        json_bytes = fin.read()                      # 3. bytes (i.e. UTF-8)
+
+    json_str = json_bytes.decode('utf-8')            # 2. string (i.e. JSON)
+    data = json.loads(json_str)                      # 1. data
+    return data
