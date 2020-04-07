05b25c5c6926e35e973d4bd2dc5f9ca81664551c
Wenhu Chen <wenhuchen@ucsb.edu>
2020-04-07 10:36:10 -0700

    changed utils

--- utils.py
+++ utils.py
@@ -31,9 +31,14 @@ def compressGZip(file_name):
         fout.write(json_bytes)
 
 def readGZip(file_name):
-    with gzip.GzipFile(file_name, 'r') as fin:    # 4. gzip
-        json_bytes = fin.read()                      # 3. bytes (i.e. UTF-8)
+    if file_name.endswith('gz'):
+        with gzip.GzipFile(file_name, 'r') as fin:    # 4. gzip
+            json_bytes = fin.read()                      # 3. bytes (i.e. UTF-8)
 
-    json_str = json_bytes.decode('utf-8')            # 2. string (i.e. JSON)
-    data = json.loads(json_str)                      # 1. data
-    return data
+        json_str = json_bytes.decode('utf-8')            # 2. string (i.e. JSON)
+        data = json.loads(json_str)                      # 1. data
+        return data
+    else:
+        with open(file_name, 'r') as fin:
+            data = json.load(fin)
+        return data
